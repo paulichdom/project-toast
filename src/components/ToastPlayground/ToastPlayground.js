fd1357c3d40c039341f444cea3095b6c604f1e8e
paulichdom <paulic.domagoj@gmail.com>
2026-01-31 09:38:54 +0100

    preview live editable toast

--- src/components/ToastPlayground/ToastPlayground.js
+++ src/components/ToastPlayground/ToastPlayground.js
@@ -1,18 +1,27 @@
-import React, { useState } from "react";
-
+import { useState } from "react";
+import Toast from "../Toast";
 import Button from "../Button";
+import { XCircle } from "react-feather";
 
 import styles from "./ToastPlayground.module.css";
 
 const VARIANT_OPTIONS = ["notice", "warning", "success", "error"];
 
 function ToastPlayground() {
-  const [message, setMessage] = useState("");
+  const [inputMessage, setInputMessage] = useState("");
+  const [toastMessage, setToastMessage] = useState("");
   const [variantOption, setVariantOption] = useState(VARIANT_OPTIONS[0]);
+  const [showToast, setShowToast] = useState(false);
+  const [showValidationMessage, setShowValidationMessage] = useState(false);
 
   const handleSetMessage = (event) => {
     const nextMessage = event.currentTarget.value;
-    setMessage(nextMessage);
+    if (showValidationMessage && nextMessage.length < 3) {
+      setShowValidationMessage(true);
+    } else {
+      setShowValidationMessage(false);
+    }
+    setInputMessage(nextMessage);
   };
 
   const handleSetVariantOption = (event) => {
@@ -22,6 +31,15 @@ function ToastPlayground() {
 
   const handleSubmit = (event) => {
     event.preventDefault();
+    if (inputMessage.length < 3) {
+      setShowValidationMessage(true);
+      return;
+    }
+
+    if (!showToast) {
+      setToastMessage(inputMessage);
+      setShowToast(true);
+    }
   };
 
   return (
@@ -30,7 +48,13 @@ function ToastPlayground() {
         <img alt="Cute toast mascot" src="/toast.png" />
         <h1>Toast Playground</h1>
       </header>
-
+      {showToast && (
+        <Toast
+          variant={variantOption}
+          message={toastMessage}
+          dismiss={() => setShowToast(false)}
+        />
+      )}
       <div className={styles.controlsWrapper}>
         <div className={styles.row}>
           <label
@@ -44,7 +68,7 @@ function ToastPlayground() {
             <textarea
               id="message"
               className={styles.messageInput}
-              value={message}
+              value={inputMessage}
               onChange={handleSetMessage}
             />
           </div>
@@ -75,7 +99,15 @@ function ToastPlayground() {
         <div className={styles.row}>
           <div className={styles.label} />
           <div className={`${styles.inputWrapper} ${styles.radioWrapper}`}>
-            <Button>Pop Toast!</Button>
+            <div>
+              <Button>Pop Toast!</Button>
+              {showValidationMessage && (
+                <p className={styles.validationMessage}>
+                  <XCircle size={13} />
+                  Message should have at least 3 characters
+                </p>
+              )}
+            </div>
           </div>
         </div>
       </div>
